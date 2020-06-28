e14cc6830e90ee884851e7d1075a0843fc76989a
Dmitry Solovov <cyradin@protonmail.com>
2020-06-28 18:00:11 +0200

    feat: Add character-get-by-id action

---
+++ src/Enum/RegexEnum.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Enum;
+
+/**
+ * Class RegexEnum
+ *
+ * @package App\Enum
+ */
+class RegexEnum
+{
+    public const UUID_V4 = '^[0-9a-f]{8}-[0-9a-f]{4}-[4][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}';
+}
